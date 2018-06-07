52c79c96a358014e91ede6a1bd2c0e49d1c9c545
Bruno Quint <b1quint@gmail.com>
2018-06-07 11:21:00 -0400

    Last modifications on PTS4

--- Examples/pts4_plots/ex3_figs_and_axes_2.py
+++ Examples/pts4_plots/ex3_figs_and_axes_2.py
@@ -11,21 +11,21 @@ x = np.arange(5) - 2
 y = x ** 2
 
 fig = plt.figure(num="My_Figure")
-#ax1 = fig.add_subplot(211)
-#ax2 = fig.add_subplot(223)
-#ax3 = fig.add_subplot(224)
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(223)
+ax3 = fig.add_subplot(224)
 
-#ax1.plot(x, y, "C0o")
-#ax1.set_xlim(-3, 3)
-#ax1.set_ylim(-1, 5)
-#ax1.set_title("Axis 1 (211)")
+ax1.plot(x, y, "C0o")
+ax1.set_xlim(-3, 3)
+ax1.set_ylim(-1, 5)
+ax1.set_title("Axis 1 (211)")
 
-#ax2.plot(x, y, "C1s")
-#ax2.set_title("Axis 2 (223)")
+ax2.plot(x, y, "C1s")
+ax2.set_title("Axis 2 (223)")
 
-#ax3.plot(x, y, "C2:")
-#ax3.set_title("Axis 3 (224")
+ax3.plot(x, y, "C2:")
+ax3.set_title("Axis 3 (224)")
 
-#fig.tight_layout()
+fig.tight_layout()
 plt.show()
 
