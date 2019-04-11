80acf39a75f63236ba0ff84933fc09b70f538e78
Bruno Quint <b1quint@gmail.com>
2019-04-11 12:58:54 -0400

    Added answer to Exercise 3

--- Exercises/basics_3_packages/my_module.py
+++ Exercises/basics_3_packages/my_module.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """ 
-This module, called my_module.py, contains a function that simulates a six side dice roll
+This module contains a function that simulates a six side dice roll
 """  
 
 import random
@@ -11,6 +11,5 @@ def roll_dice():
     result = random.randint(_min, _max)
     return result
 
-print('Current scope: {}'.format(__name__))
 print('The dice gave me {}'.format(roll_dice()))
 
